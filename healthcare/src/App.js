e257dedaa7ec30d9b36fdbd4ab017d4f7df80b32
alen <alen.pshaju@gmail.com>
2017-03-21 17:13:42 -0500

    Commit 1

--- healthcare/src/App.js
+++ healthcare/src/App.js
@@ -1,18 +1,26 @@
+/* Goals:
+1 - User authentication for multiple roles|register 
+2 - data entry capabilities. Each role log in and enter, need web forms
+3 - at least 1 or 2 triggers some type of checks
+4 - Queries and reports      
+
+Database:
+Doctor -> (Assigns) -> Prescription -> (for) -> patients
+Doctor -> Assigned_To -> patient
+Doctor -> (does) -> Diagnosis -> (on) -> Patients
+Doctor -> Works_At -> Office
+Patient -> (sets) -> Appointment -> (at) -> Office -> (submit) -> Appointment -. Doctor
+Doctor -> (does) -> Test -> (for) -> Patient
+*/
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        Hello World
       </div>
     );
   }
