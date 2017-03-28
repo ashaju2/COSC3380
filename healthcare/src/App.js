a9fdc25e647bf13783345e1fd912162b9aa962b7
mmrodri8 <mmrodriguez6@uh.edu>
2017-03-27 20:22:50 -0500

    updates
    What we worked on in class

--- healthcare/src/App.js
+++ healthcare/src/App.js
@@ -1,49 +1,4 @@
-<<<<<<< HEAD
-/* Goals:
-1 - User authentication for multiple roles|register - N/A
-2 - data entry capabilities. Each role log in and enter, need web forms - N/A
-3 - at least 1 or 2 triggers some type of checks - N/A
-4 - Queries and reports - N/A      
 
-5 - Connect nodejs in RDS to react - Work on this
-
-Database:
-Doctor -> (Assigns) -> Prescription -> (for) -> patients
-Doctor -> Assigned_To -> patient
-Doctor -> (does) -> Diagnosis -> (on) -> Patients
-Doctor -> Works_At -> Office
-Patient -> (sets) -> Appointment -> (at) -> Office -> (submit) -> Appointment -> Doctor
-Doctor -> (does) -> Test -> (for) -> Patient
-*/
-
-import React, { Component } from 'react';
-import Frontview from './Components/Frontview';
-import './App.css';
-
-class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      projects: 
-        {
-          title: 'Website',
-          category: 'Web Design'
-        }
-      
-    } 
-  }
-  render() {
-    return (
-      <div className="App">
-        <Frontview />
-        {this.state.projects.title}
-      </div>
-    );
-  }
-}
-
-export default App;
-=======
 /* Goals:
 1 - User authentication for multiple roles|register - N/A
 2 - data entry capabilities. Each role log in and enter, need web forms - N/A
@@ -62,6 +17,8 @@ Doctor -> (does) -> Test -> (for) -> Patient
 
 import React, { Component } from 'react';
 import Frontview from './Components/Frontview';
+import Login from './Components/Login';
+
 import './App.css';
 
 class App extends Component {
@@ -79,6 +36,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+		<Login />
         <Frontview />
         {this.state.projects.title}
       </div>
@@ -87,4 +45,4 @@ class App extends Component {
 }
 
 export default App;
->>>>>>> 74e3bb474eb207a906a7ada21306ad4d97cbb654
+
