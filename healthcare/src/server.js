72f864b55f0c61a461999b651ce31ecdcccca575
alen <alen.pshaju@gmail.com>
2017-03-28 22:18:01 -0500

    commit 3

--- healthcare/src/server.js
+++ healthcare/src/server.js
@@ -20,7 +20,37 @@ connection.query(sql,['Matthew'],function(err,result){
 	console.log(result);
 	
 });
+
+var sql="DELETE FROM DOCTOR WHERE Fname = ?"
+connection.query(sql,['Alen'],function(err,result){
+	
+	if(err)throw err;
+	console.log('Data deleted \n');
+	console.log(result);
+	
+});
+
+
  connection.query('INSERT INTO DOCTOR (Fname,Minit,Lname,Address,NPI,StateLicenseNumber,StateOfLicense,Specialty,PrimaryPhysician,Phone) VALUES(?,?,?,?,?,?,?,?,?,?)',['Matthew','M','Rodriguez','123 Fake Street','717835421','123456789','TX','Gastroenterologist','9999','123987654'],function(err, result){
+	 if(err) throw err;
+ console.log('Inserted data \n');
+	connection.query('SELECT * FROM DOCTOR',function(err,results){ //reading back data that i inserted 
+	    console.log(results[0].Fname);
+        console.log(results[0].Minit);
+        console.log(results[0].Lname);
+		console.log(results[0].Address);
+        console.log(results[0].NPI);
+        console.log(results[0].StateLicenseNumber);
+        console.log(results[0].StateOfLicense);
+		console.log(results[0].Specialty);
+        console.log(results[0].PrimaryPhysician);
+        console.log(results[0].Phone);
+        
+	
+	
+ })});
+
+  connection.query('INSERT INTO DOCTOR (Fname,Minit,Lname,Address,NPI,StateLicenseNumber,StateOfLicense,Specialty,PrimaryPhysician,Phone) VALUES(?,?,?,?,?,?,?,?,?,?)',['Alen','P','Shaju','123 New Fake Street','243576809','987654321','TX','Cardiologist','1111','123987654'],function(err, result){
 	 if(err) throw err;
  console.log('Inserted data \n');
 	connection.query('SELECT * FROM DOCTOR',function(err,results){ //reading back data that i inserted 
